dd6c7970f111c94b71bb1e556f1adb18ce2f0162
halkity <saito75061@gmail.com>
2020-12-22 07:31:34 +0900

    スコープ定義written_aboutの変数を修正

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,4 @@
 class Book < ApplicationRecord
-  scope :costly, -> {where("price > ?", 3000)}
-  scope :written_about, -> {where("name like ?", "%#{theme}%")}
+  scope :costly, -> {where("price > ?", 3000)}    # Book.costly
+  scope :written_about, ->(theme) {where("name like ?", "%#{theme}%")}    # Book.written_about(theme)
 end
