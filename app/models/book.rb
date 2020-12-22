46367d6dd6cb48fdc66675c87208bc2c810db451
halkity <saito75061@gmail.com>
2020-12-22 11:30:09 +0900

    publisherとbookのリレーションを追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
   scope :costly, -> {where("price > ?", 3000)}    # Book.costly
   scope :written_about, ->(theme) {where("name like ?", "%#{theme}%")}    # Book.written_about(theme)
+
+  belongs_to :publisher
 end
