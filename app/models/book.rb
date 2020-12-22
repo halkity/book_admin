5df0bff83527d5a2caf5025cb11cb97b0ec55dcf
halkity <saito75061@gmail.com>
2020-12-22 15:07:32 +0900

    bookのvalidationを追加
    ・名前の空×
    ・名前の文字数25字以下
    ・値段は0円以上
    ・値段は整数で登録

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,4 +5,10 @@ class Book < ApplicationRecord
   belongs_to :publisher
   has_many :book_authors
   has_many :authors, through: :book_authors
+
+  validates :name, presence: true
+  validates :name, length: {maximum: 25}
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  # numericality: { only_integer: true} => 整数値のみ
+  # numericality: { greater_than_or_equal_to: 0 } => 0より大きな数字
 end
